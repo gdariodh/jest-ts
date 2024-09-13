75005894d82de7fa7fd89a4605d4a62828353e94
Gabriel Diaz <diazhernandezgabriel2001@gmail.com>
2024-09-13 00:08:12 -0300

    feat: add aaa principles to unit test

--- src/__tests__/utils.test.ts
+++ src/__tests__/utils.test.ts
@@ -1,8 +1,17 @@
 import { toUpperCase } from '../app/utils';
 
+// AAA principles
+// 1. Arrange - setup the test, 2. Act - execute the test, 3. Assert - check the result
+// sut - system under test
+
 describe('Utils test suite', () => {
-  test('should return uppercase', () => {
-    const result = toUpperCase('abc');
-    expect(result).toBe('ABC');
+  it('should return uppercase of valid string', () => {
+    // arrange
+    const sut = toUpperCase;
+    const expected = 'ABC';
+    // act
+    const actual = sut('abc');
+    // assert
+    expect(actual).toBe(expected);
   });
 });
